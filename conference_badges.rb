73fda5e2e2c0c6b800cb9a0686a67b0dbbbfe1e0
Aiden leeming <aidenjl193@gmail.com>
2019-04-07 19:36:05 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  messages = []
+  for name in names do
+    messages << badge_maker(name)
+  end
+  messages
+end
+
+def assign_rooms(speakers)
+  messages = []
+  for i in 0..speakers.length - 1 do
+    messages << "Hello, #{speakers[i]}! You'll be assigned to room #{i+1}!"
+  end
+  messages
+end
+
+def printer(speakers)
+  badges = batch_badge_creator(speakers)
+  for badge in badges do
+    puts badge
+  end
+  rooms = assign_rooms(speakers)
+  for room in rooms do
+    puts room
+  end
+end
