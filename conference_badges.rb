444b9022a302ac44921983bf45d3288ad0a51b7f
Katherine Wren <kwren@outlook.com>
2019-04-16 11:20:49 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,34 @@
 # Write your code here.
+def badge_maker(name)
+ return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array2 = []
+  array.each do |name| 
+   newname = badge_maker(name) 
+    array2 << newname
+  end
+ array2
+end
+
+def assign_rooms(array)
+  array2 = []
+  array.each_with_index do |name, index|
+    index += 1
+  welcome = "Hello, #{name}! You'll be assigned to room #{index}!"
+  array2 << welcome
+end
+array2
+end
+
+def printer(firstnames)
+names = batch_badge_creator(firstnames)
+rooms = assign_rooms(firstnames)
+names.each do |greetings|
+  puts greetings
+end
+rooms.each do |assignments|
+  puts assignments
+end
+end
